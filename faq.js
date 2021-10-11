9682b569f9b1efa1a6f94344a65f5df3447c6aec
zeev-js <91810804+zeev-js@users.noreply.github.com>
2021-10-11 16:36:28 +0300

    Update faq.js

--- faq.js
+++ faq.js
@@ -1,8 +1,9 @@
-function populate(json) {
+async function populate(jsondata) {
+    let data = await fetch(jsondata);
+    data = await data.json();
     let root = document.getElementById('faqAccordion');
-    json = [{ question: "How do I?", answer: "like so...",video:"https://www.youtube.com" }, { question: "How do I?", answer: "like so..." }, { question: "How do I?", answer: "like so..." }];
-    for (let i = 0; i < json.length; i++) {
-        const item = json[i];
+    for (let i = 0; i < data.length; i++) {
+        const item = data[i];
         let newdiv = document.createElement('div');
         newdiv.className = "panel panel-default";
         newdiv.innerHTML = `<div class="panel-heading accordion-toggle collapsed question-toggle" data-toggle="collapse" data-parent="#faqAccordion" data-target="#question${i}">
@@ -12,7 +13,7 @@ function populate(json) {
         </div>
         <div id="question${i}" class="panel-collapse collapse" style="height: 0px;">
         <div class="panel-body">
-        ${item.video?`<h5><a href="${item.video}" class="label label-danger">Watch the tutorial</a></h5>`:`<h5><span class="label label-primary">Answer</span></h5>`}
+        ${item.video ? `<h5><a href="${item.video}" class="label label-danger">Watch the tutorial</a></h5>` : `<h5><span class="label label-primary">Answer</span></h5>`}
         <p>${item.answer}</p>
         </div>
         </div>
